72cad4696952a77d4811cef49f8e8ac1eb692981
Kevin Joshua <142289181+kevin-joshua@users.noreply.github.com>
2024-07-30 23:27:24 +0530

    Update Main.jsx

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -4,7 +4,7 @@ function Main(props) {
   const {data} =props;
   return (
     <div className='flex flex-col'>
-      <img src={data.url} alt={data?.title || 'bg-img'} className='min-h-full w-full object-cover'></img>
+      <img src={data.url} alt={data?.title || 'bg-img'} className='mmax-h-screen w-screen object-cover'></img>
     </div>
   )
 }
