758716ee94bd1f5076e3260a0233c664a1bbde01
Kevin Joshua <142289181+kevin-joshua@users.noreply.github.com>
2024-07-30 23:40:08 +0530

    Update Main.jsx

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function Main(props) {
   const {data} =props;
   return (
-    <div className='flex flex-col'>
-      <img src={data.url} alt={data?.title || 'bg-img'} className='lg:max-h-screen md:min-h-screen w-screen object-cover'></img>
+    <div className='relative flex flex-col min-h-screen'>
+      <img src={data.url} alt={data?.title || 'bg-img'} className='absolute top-0 left-0 min-h-screen w-screen object-cover'></img>
     </div>
   )
 }
